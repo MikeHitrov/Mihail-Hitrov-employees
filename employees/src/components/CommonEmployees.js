0e65d19396e1d34d3aea6d9d069bf0f053447a19
MikeHitrov <mike.hitrov@gmail.com>
2022-11-12 14:52:34 +0200

    Fixed display of common employees and date parser

--- employees/src/components/CommonEmployees.js
+++ employees/src/components/CommonEmployees.js
@@ -1,49 +1,74 @@
-import React, { useState } from "react";
-import './styles/CommonEmployees.css';
+import React, { useState, useEffect } from "react";
+import "./styles/CommonEmployees.css";
 
 export default function GetCommonEmployees({ data }) {
   const [common, setCommon] = useState([]);
 
+  useEffect(() => {
+    const userData = extractEmployees(data);
+    setCommon(userData);
+  });
+
   const setCommonWorkingDays = (a, b) => {
-    if (a.dateFrom <= b.dateFrom) {
-      if (a.dateTo >= b.dateTo) {
-        setCommon(
-          common.push({
-            firstEmployeeId: a.id,
-            secondEmployeeId: b.id,
-            projectId: a.projectId,
-            days: dateDiffInDays(a.dateFrom, a.dateTo),
-          })
-        );
+    if (
+      !common.some(
+        (commonItem) =>
+          ((commonItem.firstEmployeeId == a.id &&
+            commonItem.secondEmployeeId == b.id) ||
+            (commonItem.firstEmployeeId == b.id &&
+              commonItem.secondEmployeeId == a.id)) &&
+          commonItem.projectId == a.projectId &&
+          commonItem.projectId == b.projectId
+      )
+    ) {
+      if (a.dateFrom <= b.dateFrom) {
+        if (a.dateTo >= b.dateTo) {
+          let daysInCommon = dateDiffInDays(a.dateFrom, a.dateTo);
+
+          setCommon(
+            common.push({
+              firstEmployeeId: a.id,
+              secondEmployeeId: b.id,
+              projectId: a.projectId,
+              days: daysInCommon,
+            })
+          );
+        } else {
+          let daysInCommon = dateDiffInDays(a.dateFrom, a.dateTo);
+
+          setCommon(
+            common.push({
+              firstEmployeeId: a.id,
+              secondEmployeeId: b.id,
+              projectId: a.projectId,
+              days: daysInCommon,
+            })
+          );
+        }
       } else {
-        setCommon(
-          common.push({
-            firstEmployeeId: a.id,
-            secondEmployeeId: b.id,
-            projectId: a.projectId,
-            days: dateDiffInDays(a.dateFrom, b.dateTo),
-          })
-        );
-      }
-    } else {
-      if (a.dateTo >= b.dateTo) {
-        setCommon(
-          common.push({
-            firstEmployeeId: a.id,
-            secondEmployeeId: b.id,
-            projectId: a.projectId,
-            days: dateDiffInDays(b.dateFrom, a.dateTo),
-          })
-        );
-      } else {
-        setCommon(
-          common.push({
-            firstEmployeeId: a.id,
-            secondEmployeeId: b.id,
-            projectId: a.projectId,
-            days: dateDiffInDays(b.dateFrom, b.dateTo),
-          })
-        );
+        let daysInCommon = dateDiffInDays(a.dateFrom, a.dateTo);
+
+        if (a.dateTo >= b.dateTo) {
+          setCommon(
+            common.push({
+              firstEmployeeId: a.id,
+              secondEmployeeId: b.id,
+              projectId: a.projectId,
+              days: daysInCommon,
+            })
+          );
+        } else {
+          let daysInCommon = dateDiffInDays(a.dateFrom, a.dateTo);
+
+          setCommon(
+            common.push({
+              firstEmployeeId: a.id,
+              secondEmployeeId: b.id,
+              projectId: a.projectId,
+              days: daysInCommon,
+            })
+          );
+        }
       }
     }
   };
@@ -64,19 +89,16 @@ export default function GetCommonEmployees({ data }) {
     });
 
     const sorted = common.sort((a, b) => {
-      return a.commonProject.days >= b.commonProject.days ? 1 : -1;
+      return a.days >= b.days ? 1 : -1;
     });
 
     return sorted;
   };
 
-  if (common.length === 0) {
-    setCommon(extractEmployees(data));
-  }
-
   function dateDiffInDays(a, b) {
     const _MS_PER_DAY = 1000 * 60 * 60 * 24;
     // Discard the time and time-zone information.
+
     if (a !== undefined && b !== undefined) {
       const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
       const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
@@ -86,6 +108,8 @@ export default function GetCommonEmployees({ data }) {
 
     return 0;
   }
+
+  console.log(common);
 
   return (
     <table>
@@ -104,7 +128,7 @@ export default function GetCommonEmployees({ data }) {
               <td>{a.firstEmployeeId}</td>
               <td>{a.secondEmployeeId}</td>
               <td>{a.projectId}</td>
-              <td>{dateDiffInDays(a.dateFrom, a.dateTo)}</td>
+              <td>{a.days}</td>
             </tr>
           ))
         ) : (
